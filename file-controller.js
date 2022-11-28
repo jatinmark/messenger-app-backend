3a9ac614acf73b248895be342c972f56ba46256d
jatin choudhary <73272963+jatinmark@users.noreply.github.com>
2022-11-29 00:12:57 +0530

    Update file-controller.js

--- file-controller.js
+++ file-controller.js
@@ -1,7 +1,7 @@
 import  Grid   from "gridfs-stream";
 import mongoose from "mongoose";
 
- const Url = 'http://localhost:9000' ; 
+ const Url = 'https://messenger-app-backend-gkftm4epo-jatinmark.vercel.app/' ; 
 
  let gfs , gridfsbucket ;
  const conn = mongoose.connection;
